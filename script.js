fb12d23c3e78d5a4264736e9a89efe0d6b5303e4
zhmurandrii <249627766+zhmurandrii@users.noreply.github.com>
2026-02-07 17:56:51 +0200

    create different canvas on fixed size container

--- script.js
+++ script.js
@@ -1,21 +1,30 @@
 const container = document.getElementById("container");
 const smallDiv = document.createElement("div");
+const sizeButton = document.querySelector("button");
 
-const width = 340;
-const height = width;
-const widthSQRT = Math.floor(Math.sqrt(width));
-console.log(widthSQRT);
+let numOfSquares = 20;
 
-for (i = 0; i < widthSQRT ** 2; i++) {
-  addSquare();
+function createGrid() {
+  numOfSquares = +prompt("enter");
+  numOfSquares = Math.floor(numOfSquares);
+  container.textContent = "";
+  if (numOfSquares <= 0 || numOfSquares >= 100) {
+    alert("Please enter a number from 1 to 100");
+  } else {
+    for (i = 0; i < numOfSquares ** 2; i++) {
+      addSquare();
+    }
+  }
 }
+
+sizeButton.addEventListener("click", createGrid);
 
 function addSquare() {
   const smallDiv = document.createElement("div");
   smallDiv.classList.add("smallDiv");
-  smallDiv.style.width = `${100 / widthSQRT}%`;
-  smallDiv.style.height = `${100 / widthSQRT}%`;
-  smallDiv.addEventListener("mousedown", function () {
+  smallDiv.style.width = `${100 / numOfSquares}%`;
+  smallDiv.style.height = `${100 / numOfSquares}%`;
+  smallDiv.addEventListener("mouseover", function () {
     smallDiv.classList.add("changeColor");
   });
   container.appendChild(smallDiv);
