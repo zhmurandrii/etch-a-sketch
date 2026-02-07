9b4936cdb0ba740c67a743e5614a76e88591a168
zhmurandrii <249627766+zhmurandrii@users.noreply.github.com>
2026-02-07 18:23:41 +0200

    add random color option

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
 const container = document.getElementById("container");
 const smallDiv = document.createElement("div");
 const sizeButton = document.querySelector("button");
+const randomColorButton = document.getElementById("randomColor");
+let colorfullDiv = false;
 
+randomColorButton.addEventListener("click", () => {
+  randomColorButton.textContent = colorfullDiv
+    ? "USE RANDOM COLOR"
+    : "USE BLACK COLOR";
+  if (colorfullDiv) {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    currentRandomColor = `rgb(${r}, ${g}, ${b})`;
+  }
+  return (colorfullDiv = !colorfullDiv);
+});
+
+sizeButton.addEventListener("click", createGrid);
 let numOfSquares = 20;
+let currentRandomColor = "";
 
 function createGrid() {
   numOfSquares = +prompt("enter");
@@ -17,15 +34,18 @@ function createGrid() {
   }
 }
 
-sizeButton.addEventListener("click", createGrid);
-
 function addSquare() {
   const smallDiv = document.createElement("div");
   smallDiv.classList.add("smallDiv");
   smallDiv.style.width = `${100 / numOfSquares}%`;
   smallDiv.style.height = `${100 / numOfSquares}%`;
   smallDiv.addEventListener("mouseover", function () {
-    smallDiv.classList.add("changeColor");
+    if (colorfullDiv === false) {
+      smallDiv.style.backgroundColor = "";
+      smallDiv.classList.add("changeColor");
+    } else {
+      smallDiv.style.backgroundColor = currentRandomColor;
+    }
   });
   container.appendChild(smallDiv);
 }
