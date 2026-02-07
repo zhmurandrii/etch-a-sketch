c6b7f89b3e91853f4fa62c78a266c56da039e3cb
zhmurandrii <249627766+zhmurandrii@users.noreply.github.com>
2026-02-07 17:18:40 +0200

    create perfect squares inside container

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
 const container = document.getElementById("container");
 const smallDiv = document.createElement("div");
 
-for (i = 0; i < 16 * 16; i++) {
+const width = 340;
+const height = width;
+const widthSQRT = Math.floor(Math.sqrt(width));
+console.log(widthSQRT);
+
+for (i = 0; i < widthSQRT ** 2; i++) {
   addSquare();
 }
 
 function addSquare() {
   const smallDiv = document.createElement("div");
   smallDiv.classList.add("smallDiv");
+  smallDiv.style.width = `${100 / widthSQRT}%`;
+  smallDiv.style.height = `${100 / widthSQRT}%`;
   smallDiv.addEventListener("mousedown", function () {
     smallDiv.classList.add("changeColor");
   });
